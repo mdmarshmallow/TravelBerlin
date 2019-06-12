9de2dea3811be51fac5584b44c8e23c09daf0a7e
Lucas Kiefer <luckie@client-141-23-149-231.wlan.tu-berlin.de>
2019-06-12 10:56:45 +0200

    edited home page

--- ui/src/components/Home.js
+++ ui/src/components/Home.js
@@ -23,7 +23,30 @@ class Home extends Component {
     render() {
       return (
         <div className="Home">
-        <h1>Home page</h1>
+          <h1>Home page</h1>
+          <p>Login:</p>
+          <form>
+            <label>
+              User Name:</label>
+              <input type="text" name="userName" /><br></br>
+              Password:
+              <input type="password" name="password"/><br></br>
+            <input type="submit" value="Login" />
+          </form>
+
+          <p>Register:</p>
+          <form>
+            <label>First Name:</label>
+            <input type="text" name="firstName" /><br></br>
+             Last Name:
+            <input type="text" name="lastName" /><br></br>
+            User Name:
+            <input type="text" name="userName" /><br></br>
+            Email:
+            <input type="text" name="email" /><br></br>
+            
+            <input type="submit" value="Register"/>
+          </form>
         </div>
     );
     }
