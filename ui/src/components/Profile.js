24df26cf6dbbc116628000455ff25135d456e0d8
Sanjana Jampana <sjampana7@gatech.edu>
2019-06-12 13:13:36 +0200

    profile

--- ui/src/components/Profile.js
+++ ui/src/components/Profile.js
@@ -24,6 +24,16 @@ class Profile extends Component {
       return (
         <div className="Profile">
         <h1>Profile page</h1>
+        <h2>Edit Profile:</h2>
+        <form action="/name" method= "post">
+          <label>
+            Name:</label>
+            <input type ="text" name="name" /><br></br>
+            User Name:
+            <input type ="text" name="userName" /><br></br>
+            Password:
+            <input type ="text" name="password" /><br></br>
+        </form>
         </div>
     );
     }
