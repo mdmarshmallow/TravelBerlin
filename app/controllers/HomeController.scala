4c8315b2fd50c9b651253e75e06a85a1f77b284c
Marc D'Mello <marcd2000@gmail.com>
2019-06-13 21:52:10 +0200

    Attractions render from database information

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -5,8 +5,6 @@ import javax.inject._
 import play.api.libs.json._
 import play.api.mvc._
 
-import java.io.FileInputStream
-
 import models.Attraction
 
 @Singleton
@@ -46,6 +44,33 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
 
     case class AttractionJson(name: String, location: String, description: String)
 
-    Ok
+    case class AttractionListJson(attractions: Seq[AttractionJson])
+
+    //TODO: Fix warnings
+    implicit val AttractionJsonWrites = new Writes[AttractionJson] {
+      def writes(attraction: AttractionJson) = Json.obj(
+        "name" -> attraction.name,
+        "location" -> attraction.location,
+        "description" -> attraction.description
+      )
+    }
+
+    implicit  val AttractionListJsonWrites = new Writes[AttractionListJson] {
+      def writes(attractionList: AttractionListJson) = Json.obj(
+        "attractions" -> attractionList.attractions
+      )
+    }
+
+    val attractionSeq = for (attraction <- attractionList) yield {
+      AttractionJson(attraction("name"), attraction("location"), attraction("description"))
+    }
+
+    val attractionListJson = AttractionListJson(attractionSeq)
+
+    val json: JsValue = Json.toJson(attractionListJson)
+
+    println(json)
+
+    Ok(Json.obj("content" -> Json.stringify(json)))
   }
 }
