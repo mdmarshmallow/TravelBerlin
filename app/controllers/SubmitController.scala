0c8e29c968ab2685b45b7d1a30588b8c08d54de0
Simar Kareer <skareer@outlook.com>
2019-06-13 20:51:45 +0200

    my submit controller

--- app/controllers/SubmitController.scala
+++ app/controllers/SubmitController.scala
@@ -24,21 +24,22 @@ class SubmitController @Inject()(cc: ControllerComponents) extends AbstractContr
     def register = Action (parse.json) { request: Request[JsValue] =>
         // val body: Map[String, Seq[String]] = request.body
 
-        try{
-            val firstName = (request.body \ "firstName").asOpt[String].drop(0).head
-            val lastName = (request.body \ "lastName").asOpt[String].drop(0).head
-            val userName = (request.body \ "userName").asOpt[String].drop(0).head
-            val email = (request.body \ "email").asOpt[String].drop(0).head
-            val password = (request.body \ "password").asOpt[String].drop(0).head
-        } catch {
-            case nse: NoSuchElementException => throw(nse)
-        }
+        // try{
+        //     val firstName = (request.body \ "firstName").asOpt[String].drop(0).head
+        //     val lastName = (request.body \ "lastName").asOpt[String].drop(0).head
+        //     val userName = (request.body \ "userName").asOpt[String].drop(0).head
+        //     val email = (request.body \ "email").asOpt[String].drop(0).head
+        //     val password = (request.body \ "password").asOpt[String].drop(0).head
+        // } catch {
+        //     case nse: NoSuchElementException => throw(nse)
+        // }
         // val lastName = body.get("lastName").get(0)
         // val userName = body.get("userName").get(0)
         // val email = body.get("email").get(0)
         // val password = body.get("password").get(0)
-        User.createUser(firstName, lastName, username, email, password, false)
+        // User.createUser(firstName, lastName, username, email, password, false)
         // val x = (request.body \ "answer").asOpt[String].drop(0).head
+        val x = (request.body \ "answer").writes[Int]
         println(x)
         println("yah yeet")
         Ok
