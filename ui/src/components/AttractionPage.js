294559f124a679d1837860a26c5315ef507cadcc
Sneha Kadiyala <skadiyala3@gatech.edu>
2019-07-15 13:53:29 +0200

    Delete functionality and create Comment

--- ui/src/components/AttractionPage.js
+++ ui/src/components/AttractionPage.js
@@ -8,6 +8,8 @@ class AttractionPage extends Component {
       super(props);
       this.state = {title: '',loading:true, comments: this.props.comments};
       this.handleInputChange=this.handleInputChange.bind(this);
+      this.handleRateChange = this.handleRateChange.bind(this);
+      this.handleReviewChange = this.handleReviewChange.bind(this);
     }
     reviewShow = dimmer => () => this.setState({ dimmer, reviewOpen: true, reviewFormSuccess: false})
     reviewClose = () => this.setState({ reviewOpen: false})
@@ -54,31 +56,37 @@ class AttractionPage extends Component {
       )
     }
 
-    //NEED TO ADD
-    /*
-    async componentDidUpdate() {
-        Client.getComment(comments => {
-        this.setState({
-          reviews: JSON.parse(comments.content).comments
-        });
-      });
-    }
-     */
 
     handleInputChange(event) {
         const target = event.target;
         const value = target.value;
         const name = target.name;
-  
         this.setState({
           [name]: value
         });
+    }
+
+    handleReviewChange(event) {
+      this.setState({comText: event.target.value})
+      console.log(this.state)
+    }
+
+    handleRateChange = (e) => {
+      if(e != undefined) {
+        var rating = e.rating
+        this.setState({ rating})
+      }
+    }
+
+    deleteComment() {
+      Client.sendForm({name: this.state.name, author: this.state.userEmail}, "/api/deleteComment");
     }
 
     createComment(comments) {
       let commentRendered = []
       for(var comm of comments.values()) {
         var arr = Array.from(comm.values())
+        console.log(this.state.userEmail == arr[0])
         commentRendered.push(
           <Comment>
             <Comment.Content>
@@ -88,11 +96,11 @@ class AttractionPage extends Component {
               </Comment.Metadata>
               <Comment.Text>{arr[1]}</Comment.Text>
               <Comment.Actions>
-                {this.state.admin &&//USERS ARE EQUAL
+                {(this.state.admin || this.state.userEmail == arr[0]) &&
                 <Comment.Action color="red"><Icon name="edit"></Icon>Edit</Comment.Action>
                 }
-                {this.state.admin &&
-                <Comment.Action color="red"><Icon name="delete" color="red"></Icon>Delete</Comment.Action>
+                {this.state.admin || this.state.userEmail == arr[0] &&
+                <Comment.Action color="red" onClick={this.deleteComment()}><Icon name="delete" color="red"></Icon>Delete</Comment.Action>
                 }
               </Comment.Actions>
             </Comment.Content>
@@ -103,6 +111,14 @@ class AttractionPage extends Component {
       return commentRendered;
     }
 
+    submitCommentChanges = () => {
+      const comment = {name: this.state.name, author: this.state.userEmail, comment: this.state.comText, rating: this.state.rating}
+      Client.sendForm(comment, "/api/createComment").then(console.log)
+    }
+
+    handleEdit = () => {
+
+    }
     submitChanges = () => {
         const editedAttraction = {id: parseInt(this.props.match.params.id), name: this.state.name, location: this.state.editedLocation, description: this.state.editedDescription, imageUrl: this.state.editedImageUrl}
         Client.sendForm(editedAttraction, "/api/editAttraction").then((json => {
@@ -153,12 +169,12 @@ class AttractionPage extends Component {
 
                               {this.state.loggedin &&
 
-                                <Modal open={this.state.reviewOpen} onSubmit={() => { this.submitChanges() }} onClose={this.reviewClose} trigger={<div onClick={(e) => e.preventDefault()} className="ui primary button" onClick={this.reviewShow('blurring')}>Add Review</div>}>
+                                <Modal open={this.state.reviewOpen} onSubmit={() => { this.submitCommentChanges() }} onClose={this.reviewClose} trigger={<div onClick={(e) => e.preventDefault()} className="ui primary button" onClick={this.reviewShow('blurring')}>Add Review</div>}>
                                 <Modal.Header>Add Review</Modal.Header>
                                 <Modal.Content>
                                 <Form reply>
-                                  <Form.Field control={TextArea} label='Content' placeholder='What did you think?' />
-                                  <Rating label="Rating" maxRating={5} defaultRating={0} icon="star" size='large' color="black" clearable/>
+                                  <Form.Field control={TextArea} label='Content' placeholder='What did you think?' value={this.state.comText} onChange={this.handleReviewChange}/>
+                                  {this.state.reviewOpen && <Rating label="Rating" maxRating={5} defaultRating={0} icon="star" size='large' color="black" onRate={this.handleRateChange()}/>}
                                   <br></br>
                                   <Button content='Add Review' labelPosition='left' icon='edit' primary />
                                 </Form>
